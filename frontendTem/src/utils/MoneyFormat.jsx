f3a1430f2ae0cf7b308d4bbaff8d62078f74830c
samir <samyrtusna@gmail.com>
2024-10-11 20:47:38 +0100

    fourth commit

--- frontendTem/src/utils/MoneyFormat.jsx
+++
@@ -1,9 +0,0 @@
-function FormatMoney(value) {
-  return new Intl.NumberFormat("fr-FR", {
-    style: "decimal",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(value);
-}
-
-export default FormatMoney;
